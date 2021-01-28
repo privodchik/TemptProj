52bd3a76e79d45efa5c6323cd538b5c4c3bc5406
Dmitry Ivanov <dmitsi@rambler.ru>
2021-01-28 18:29:26 +0300

    add picture on button clear

--- TemptProj/MainWindow.xaml.cs
+++ TemptProj/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO.Ports;
 
 namespace TemptProj
 {
@@ -20,9 +21,42 @@ namespace TemptProj
     /// </summary>
     public partial class MainWindow : Window
     {
+        private SerialPort m_serialPort;
+        
         public MainWindow()
         {
             InitializeComponent();
+
+            btnConnect.Tag = false;
+
+            string[] _availableSerialPots = SerialPort.GetPortNames();
+
+            if (_availableSerialPots.Length != 0)
+            {
+                Array.Sort(_availableSerialPots);
+
+                foreach (string x in _availableSerialPots)
+                    cmbPort.Items.Add(x);
+
+                cmbPort.SelectedIndex = 0;
+            }
+        }
+
+        private void btnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            if (Convert.ToBoolean(btnConnect.Tag) == false)
+            {
+                try{
+                    m_serialPort = new SerialPort(Convert.ToString(cmbPort.SelectedValue), Convert.ToInt32(cmbBaud.SelectedValue), Parity.None, 8, StopBits.One);
+                    m_serialPort.Open();
+                }
+                catch
+                {
+
+                }
+                btnConnect.Tag = true;
+                btnConnect.Content = "Disconnect";
+            }
         }
     }
 }
